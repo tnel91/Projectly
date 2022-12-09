40b82597de40bb704f060d8d49ab790a45aeb415
Travis Nelson <tjnel6691@gmail.com>
2022-12-09 09:46:15 -0500

    Added components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Landing from './pages/Landing'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Profile from './pages/Profile'
+import ProjectDetails from './pages/ProjectDetails'
+import Signup from './pages/Signup'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/project/:projectId" element={<ProjectDetails />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,5 @@
+const Landing = () => {
+  return <div>landing</div>
+}
+
+export default Landing
