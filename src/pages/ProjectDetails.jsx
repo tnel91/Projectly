d31635785ab738eb121a885375eaae50cf9c98ec
Travis Nelson <tjnel6691@gmail.com>
2022-12-10 18:36:00 -0500

    Added hooks/getProjectDetails to project details page

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,5 +1,31 @@
-const ProjectDetails = () => {
-  return <div>ProjectDetails</div>
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const ProjectDetails = (props) => {
+  let { projectId } = useParams()
+  const [details, setDetails] = useState(null)
+
+  const getProjectDetails = async () => {
+    await axios
+      .get(`${BASE_URL}/projects/${projectId}`)
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getProjectDetails()
+  }, [])
+  return (
+    <div>
+      <p>stuff</p>
+    </div>
+  )
 }
 
 export default ProjectDetails
