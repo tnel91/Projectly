2a0874c7942d25325389b649e49490873d1c5d50
Travis Nelson <tjnel6691@gmail.com>
2022-12-10 19:27:44 -0500

    Loaded more details into page

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -5,25 +5,67 @@ import { BASE_URL } from '../globals'
 
 const ProjectDetails = (props) => {
   let { projectId } = useParams()
-  const [details, setDetails] = useState(null)
+  const [details, setDetails] = useState({
+    name: '',
+    owner: '',
+    type: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    budget: '',
+    materials: [],
+    images: []
+  })
 
   const getProjectDetails = async () => {
-    await axios
+    const response = await axios
       .get(`${BASE_URL}/projects/${projectId}`)
       .then((response) => {
         console.log(response.data)
+        return response.data
       })
       .catch((error) => {
         console.log(error)
       })
+    setDetails({
+      name: response.projectName,
+      owner: response.owner.userName,
+      type: response.projectType,
+      description: response.description,
+      startDate: response.startDate,
+      endDate: response.endDate,
+      budget: response.budget,
+      materials: response.materials.list,
+      images: response.images
+    })
   }
 
   useEffect(() => {
     getProjectDetails()
   }, [])
+
   return (
     <div>
-      <p>stuff</p>
+      <h2>Project Name: {details.name}</h2>
+      <p>Creator: {details.owner}</p>
+      <p>Description: {details.description}</p>
+      <div>
+        <h4>Materials</h4>
+        {details.materials.map((material, i) => (
+          <div key={i}>
+            <h5>{material.name}</h5>
+            <p>{material.amount}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h4>Images</h4>
+        {details.images.map((image, i) => (
+          <div key={i}>
+            <img src={image} alt="Image" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
