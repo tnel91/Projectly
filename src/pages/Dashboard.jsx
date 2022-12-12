3f6b64d7b6ee3121b2bfe019bfc7170f68d4b9de
Travis Nelson <tjnel6691@gmail.com>
2022-12-12 10:52:23 -0500

    Added useState for userProjects

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,11 +8,22 @@ import { BASE_URL } from '../globals'
 const Dashboard = ({ user, authenticated }) => {
   let navigate = useNavigate()
   const [publicProjects, setPublicProjects] = useState([])
+  const [userProjects, setUserProjects] = useState([])
 
-  const getPublicRecipes = async () => {
+  const getPublicProjects = async () => {
     await Client.get(`${BASE_URL}/projects`)
       .then((response) => {
         setPublicProjects(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const getUserProjects = async () => {
+    await Client.get(`${BASE_URL}/projects/user/${user.id}`)
+      .then((response) => {
+        setUserProjects(response.data)
       })
       .catch((error) => {
         console.log(error)
@@ -23,16 +34,43 @@ const Dashboard = ({ user, authenticated }) => {
     navigate(`/project/${id}`)
   }
 
+  const createNewProject = async () => {
+    let newProject = {
+      userId: user.id,
+      projectName: 'New Project',
+      projectType: 'test',
+      materials: {
+        list: []
+      },
+      images: [],
+      isPublic: true
+    }
+    await Client.post('/projects', newProject)
+      .then((response) => {
+        console.log(response.data.id)
+        navigate(`/project/${response.data.id}`)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   useEffect(() => {
-    getPublicRecipes()
-  }, [])
+    if (publicProjects.length === 0) {
+      getPublicProjects()
+    }
+    if (user) {
+      getUserProjects()
+    }
+  }, [user])
+
   return user && authenticated ? (
     <div>
       <header>
-        <button>Create New Project</button>
+        <button onClick={createNewProject}>Create New Project</button>
       </header>
-      <div className="row">
-        <section className="container p-5">
+      <section id="project-section" className="r">
+        <div className="container p-5">
           <h2>Public Projects</h2>
           <div id="public_projects">
             {publicProjects.map((project) => (
@@ -40,29 +78,29 @@ const Dashboard = ({ user, authenticated }) => {
                 <ProjectCard
                   id={project.id}
                   name={project.projectName}
-                  owner={project.owner.userName}
+                  owner={project.owner.username}
                   onClick={navProjectDetails}
                 />
               </div>
             ))}
           </div>
-        </section>
-        <section>
+        </div>
+        <div className="container p-5">
           <h2>My Projects</h2>
           <div id="my_projects">
-            {publicProjects.map((project) => (
+            {userProjects.map((project) => (
               <div key={project.id}>
                 <ProjectCard
                   id={project.id}
                   name={project.projectName}
-                  owner={project.owner.userName}
+                  owner={project.owner.username}
                   onClick={navProjectDetails}
                 />
               </div>
             ))}
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   ) : (
     <div>
