27c89bc8af0091aba5eb0353066e620e46363865
Travis Nelson <tjnel6691@gmail.com>
2022-12-12 11:04:47 -0500

    renamed App.css to index.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './index.css'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
