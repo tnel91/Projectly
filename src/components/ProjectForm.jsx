c96ac3a3e1635368decb9ff22e16e83da7f1214b
Travis Nelson <tjnel6691@gmail.com>
2022-12-13 01:14:47 -0500

    Moved form into its own component

---
+++ src/components/ProjectForm.jsx
@@ -0,0 +1,172 @@
+import { useState, useEffect } from 'react'
+
+const ProjectForm = ({ details, editMode, handleChange, handleCheckbox }) => {
+  const [imgUrl, setImgUrl] = useState('')
+
+  const [materialForm, setMaterialForm] = useState({
+    name: '',
+    amount: ''
+  })
+
+  const handleMatChange = (e) => {
+    setMaterialForm({ ...materialForm, [e.target.id]: e.target.value })
+  }
+
+  const handleUrlChange = (e) => {
+    setImgUrl(e.target.value)
+  }
+
+  const addMaterial = (e) => {
+    e.preventDefault()
+    details.materials.push(materialForm)
+    setMaterialForm({
+      name: '',
+      amount: ''
+    })
+  }
+
+  const addImage = (e) => {
+    e.preventDefault()
+    details.images.push(imgUrl)
+    setImgUrl('')
+  }
+
+  return editMode ? (
+    <div>
+      <form>
+        <div className="form-floating">
+          <input
+            id="projectName"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="Project Name"
+            value={details.projectName}
+            required
+          />
+          <label htmlFor="projectName">Project Name</label>
+        </div>
+        <div className="form-floating">
+          <input
+            id="description"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="Description"
+            value={details.description}
+          />
+          <label htmlFor="description">Description</label>
+        </div>
+        <div className="form-floating">
+          <input
+            id="tags"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="Tags"
+            value={details.tags}
+          />
+          <label htmlFor="tags">Tags (separate by commas):</label>
+        </div>
+        <div className="form-floating">
+          <input
+            id="budget"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="Budget"
+            value={details.budget}
+          />
+          <label htmlFor="budget">Budget:</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="date"
+            id="startDate"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="Start Date"
+            value={details.startDate}
+          />
+          <label htmlFor="startDate">Start Date:</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="date"
+            id="endDate"
+            className="form-control"
+            onChange={handleChange}
+            placeholder="End Date"
+            value={details.endDate}
+          />
+          <label htmlFor="endDate">End Date:</label>
+        </div>
+        <div className="checkbox mb-3">
+          <label>
+            <input
+              id="isPublic"
+              type="checkbox"
+              checked={details.isPublic}
+              onChange={handleCheckbox}
+            />
+            Viewable by other users?
+          </label>
+        </div>
+      </form>
+      {/* //////////////////////////////////////////// */}
+      <section id="material-section">
+        <h4>Materials</h4>
+        <form id="material-form" onSubmit={addMaterial}>
+          <div className="form-floating">
+            <input
+              id="name"
+              className="form-control"
+              onChange={handleMatChange}
+              placeholder="Material Name:"
+              value={materialForm.name}
+              required
+            />
+            <label htmlFor="name">Material Name:</label>
+          </div>
+          <div className="form-floating">
+            <input
+              id="amount"
+              className="form-control"
+              onChange={handleMatChange}
+              placeholder="Amount:"
+              value={materialForm.amount}
+              required
+            />
+            <label htmlFor="amount">Amount:</label>
+          </div>
+          <button type="submit" form="material-form">
+            Add Material
+          </button>
+        </form>
+      </section>
+      <section id="image-section">
+        <h4>Images</h4>
+        <form id="image-form" onSubmit={addImage}>
+          <div className="form-floating">
+            <input
+              id="img-url"
+              className="form-control"
+              onChange={handleUrlChange}
+              placeholder="Image URL:"
+              value={imgUrl}
+              required
+            />
+            <label htmlFor="img-url">Image URL:</label>
+          </div>
+          <button type="submit" form="image-form">
+            Add Image
+          </button>
+        </form>
+      </section>
+    </div>
+  ) : (
+    <div>
+      <h2>Project Name: {details.projectName}</h2>
+      <p>Creator: {details.owner}</p>
+      <p>Description: {details.description}</p>
+    </div>
+  )
+}
+
+export default ProjectForm
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import Client from '../services/api'
 import { BASE_URL } from '../globals'
 import ImageCard from '../components/imageCard'
+import ProjectForm from '../components/ProjectForm'
 
 const ProjectDetails = ({ user, authenticated }) => {
   let navigate = useNavigate()
@@ -11,12 +12,12 @@ const ProjectDetails = ({ user, authenticated }) => {
 
   const [editMode, setEditMode] = useState(false)
 
-  const [imgUrl, setImgUrl] = useState('')
+  // const [imgUrl, setImgUrl] = useState('')
 
-  const [materialForm, setMaterialForm] = useState({
-    name: '',
-    amount: ''
-  })
+  // const [materialForm, setMaterialForm] = useState({
+  //   name: '',
+  //   amount: ''
+  // })
 
   const [details, setDetails] = useState({
     owner: '',
@@ -66,35 +67,12 @@ const ProjectDetails = ({ user, authenticated }) => {
     setDetails({ ...details, [e.target.id]: e.target.value })
   }
 
-  const handleMatChange = (e) => {
-    setMaterialForm({ ...materialForm, [e.target.id]: e.target.value })
-  }
-
-  const handleUrlChange = (e) => {
-    setImgUrl(e.target.value)
-  }
-
   const handleCheckbox = (e) => {
     if (details.isPublic === false) {
       setDetails({ ...details, [e.target.id]: true })
     } else {
       setDetails({ ...details, [e.target.id]: false })
     }
-  }
-
-  const addMaterial = (e) => {
-    e.preventDefault()
-    details.materials.push(materialForm)
-    setMaterialForm({
-      name: '',
-      amount: ''
-    })
-  }
-
-  const addImage = (e) => {
-    e.preventDefault()
-    details.images.push(imgUrl)
-    setImgUrl('')
   }
 
   const toggleEditMode = () => {
@@ -111,7 +89,7 @@ const ProjectDetails = ({ user, authenticated }) => {
   }
 
   const saveProject = () => {
-    setEditMode((current) => !current)
+    toggleEditMode()
     setTimeout(() => {
       let editButton = document.getElementById('edit-project-button')
       editButton.removeAttribute('hidden')
@@ -129,169 +107,30 @@ const ProjectDetails = ({ user, authenticated }) => {
     }
   }, [user, details.id])
 
-  if (user && authenticated) {
-    return editMode ? (
-      // Renders if project owner clicks the Edit button
-      <div>
-        <button onClick={saveProject}>Save (just toggles for now)</button>
-        <form>
-          <div className="form-floating">
-            <input
-              id="projectName"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="Project Name"
-              value={details.projectName}
-              required
-            />
-            <label htmlFor="projectName">Project Name</label>
-          </div>
-          <div className="form-floating">
-            <input
-              id="description"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="Description"
-              value={details.description}
-            />
-            <label htmlFor="description">Description</label>
-          </div>
-          <div className="form-floating">
-            <input
-              id="tags"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="Tags"
-              value={details.tags}
-            />
-            <label htmlFor="tags">Tags (separate by commas):</label>
-          </div>
-          <div className="form-floating">
-            <input
-              id="budget"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="Budget"
-              value={details.budget}
-            />
-            <label htmlFor="budget">Budget:</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="date"
-              id="startDate"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="Start Date"
-              value={details.startDate}
-            />
-            <label htmlFor="startDate">Start Date:</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="date"
-              id="endDate"
-              className="form-control"
-              onChange={handleChange}
-              placeholder="End Date"
-              value={details.endDate}
-            />
-            <label htmlFor="endDate">End Date:</label>
-          </div>
-          <div className="checkbox mb-3">
-            <label>
-              <input
-                id="isPublic"
-                type="checkbox"
-                checked={details.isPublic}
-                onChange={handleCheckbox}
-              />
-              Viewable by other users?
-            </label>
-          </div>
-        </form>
-        {/* //////////////////////////////////////////// */}
-        <section id="material-section">
-          <h4>Materials</h4>
-          <form id="material-form" onSubmit={addMaterial}>
-            <div className="form-floating">
-              <input
-                id="name"
-                className="form-control"
-                onChange={handleMatChange}
-                placeholder="Material Name:"
-                value={materialForm.name}
-                required
-              />
-              <label htmlFor="name">Material Name:</label>
-            </div>
-            <div className="form-floating">
-              <input
-                id="amount"
-                className="form-control"
-                onChange={handleMatChange}
-                placeholder="Amount:"
-                value={materialForm.amount}
-                required
-              />
-              <label htmlFor="amount">Amount:</label>
-            </div>
-            <button type="submit" form="material-form">
-              Add Material
-            </button>
-          </form>
-          <div>
-            {details.materials.map((material, i) => (
-              <div key={i}>
-                <h5>{material.name}</h5>
-                <p>{material.amount}</p>
-              </div>
-            ))}
-          </div>
-        </section>
-        <section id="image-section">
-          <h4>Images</h4>
-          <form id="image-form" onSubmit={addImage}>
-            <div className="form-floating">
-              <input
-                id="img-url"
-                className="form-control"
-                onChange={handleUrlChange}
-                placeholder="Image URL:"
-                value={imgUrl}
-                required
-              />
-              <label htmlFor="img-url">Image URL:</label>
-            </div>
-            <button type="submit" form="image-form">
-              Add Image
-            </button>
-          </form>
-          <div>
-            {details.images.map((image, i) => (
-              <div key={i}>
-                <ImageCard
-                  i={i}
-                  image={image}
-                  editMode={editMode}
-                  images={details.images}
-                />
-              </div>
-            ))}
-          </div>
-        </section>
-      </div>
-    ) : (
-      // Renders initially for logged in user.
-      <div>
-        <button id="edit-project-button" hidden onClick={toggleEditMode}>
+  return user && authenticated ? (
+    <div>
+      <div className="header row m-2">
+        <button
+          className="col-1"
+          id="edit-project-button"
+          hidden
+          onClick={toggleEditMode}
+        >
           Edit
         </button>
-        <h2>Project Name: {details.projectName}</h2>
-        <p>Creator: {details.owner}</p>
-        <p>Description: {details.description}</p>
+        <button className="col-1">Cancel</button>
+        <button className="col-3" onClick={saveProject}>
+          Save (just toggles for now)
+        </button>
+      </div>
+      <ProjectForm
+        details={details}
+        editMode={editMode}
+        handleChange={handleChange}
+        handleCheckbox={handleCheckbox}
+      />
+      <section id="material-section">
         <div>
-          <h4>Materials</h4>
           {details.materials.map((material, i) => (
             <div key={i}>
               <h5>{material.name}</h5>
@@ -299,25 +138,29 @@ const ProjectDetails = ({ user, authenticated }) => {
             </div>
           ))}
         </div>
+      </section>
+      <section id="image-section">
         <div>
-          <h4>Images</h4>
           {details.images.map((image, i) => (
             <div key={i}>
-              <img src={image} alt="Image" />
+              <ImageCard
+                i={i}
+                image={image}
+                editMode={editMode}
+                images={details.images}
+              />
             </div>
           ))}
         </div>
-      </div>
-    )
-  } else {
-    return (
-      // Renders if not logged in.
-      <div>
-        <h3>You must be signed in to use this feature.</h3>
-        <button onClick={() => navigate('/')}>Sign In</button>
-      </div>
-    )
-  }
+      </section>
+    </div>
+  ) : (
+    // Renders if not logged in.
+    <div>
+      <h3>You must be signed in to use this feature.</h3>
+      <button onClick={() => navigate('/')}>Sign In</button>
+    </div>
+  )
 }
 
 export default ProjectDetails
