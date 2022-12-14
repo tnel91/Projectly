1a0ad2e009c98f5625398efa090bdc05d54f9a6e
Travis Nelson <tjnel6691@gmail.com>
2022-12-14 17:10:29 -0500

    Renamed NODE_ENV to REACT_APP_NODE_ENV

--- src/globals/index.js
+++ src/globals/index.js
@@ -1,5 +1,6 @@
-export let BASE_URL = 'https://project-planner-backend.herokuapp.com/api'
-// export let BASE_URL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://project-planner-backend.herokuapp.com/api'
-//     : 'http://localhost:3001/api'
+// export let BASE_URL = 'https://project-planner-backend.herokuapp.com/api'
+// export let BASE_URL = 'http://localhost:3001/api'
+export let BASE_URL =
+  process.env.REACT_APP_NODE_ENV === 'production'
+    ? 'https://project-planner-backend.herokuapp.com/api'
+    : 'http://localhost:3001/api'
