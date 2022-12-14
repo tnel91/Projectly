b53d77d9949657e725613a67b473e14b795bfcd0
Travis Nelson <tjnel6691@gmail.com>
2022-12-14 14:58:09 -0500

    Added backend url string to globals

--- src/globals/index.js
+++ src/globals/index.js
@@ -1,3 +1,5 @@
 // export let BASE_URL = 'http://localhost:3001/api'
 export let BASE_URL =
-  process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3001/api'
+  process.env.NODE_ENV === 'production'
+    ? 'https://project-planner-backend.herokuapp.com/api'
+    : 'http://localhost:3001/api'
