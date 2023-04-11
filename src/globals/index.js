ca0b26a0fa5eb3c62ad6c33861c630879a05f577
Travis Nelson <tjnel6691@gmail.com>
2023-04-10 23:54:54 -0400

    fixed globals

--- src/globals/index.js
+++ src/globals/index.js
@@ -1,4 +1,4 @@
 export let BASE_URL =
-  process.env.NODE_ENV === 'production'
+  process.env.REACT_APP_NODE_ENV === 'production'
     ? 'https://project-planner-backend.herokuapp.com/api'
     : 'http://localhost:3001/api'
